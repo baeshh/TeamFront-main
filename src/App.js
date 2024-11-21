009ab3b963b66b0e94a3382edaf402116d3b5b5a
baesh <baesh6778@gmail.com>
2024-11-21 17:38:30 +0900

    1121 메타버스 연결

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Messages from './components/Messages/Messages';
 import { Upload } from '@mui/icons-material';
 import Profile from './Profile';
 import Search from './Search';
+// Import your components here
 export default function App() {
   return (
     <Router>
@@ -16,7 +17,7 @@ export default function App() {
           <Route path="/search" element={<Search/>} />
           <Route path="/metaverse" element={<h1>피드</h1>} />
           <Route path="/messages" element={<Messages/>} />
-          <Route path="/upload" element={<h1>게시물 올리기</h1>} />
+          <Route path="/upload" element={<h1>게시물 업로드</h1>} />
           <Route path="/profile" element={<Profile/>} />
         </Routes>
       </div>
